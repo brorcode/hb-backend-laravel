75296722417bf82aab0bccb486e4cbb2b6063fdd
Sergey <sbratyshov@gmail.com>
2024-10-20 16:53:06 +0300

    Add backup commands

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,6 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+Schedule::command('backup:clean --disable-notifications')->daily()->at('01:00');
+Schedule::command('backup:run --only-db --disable-notifications')->daily()->at('01:30');
